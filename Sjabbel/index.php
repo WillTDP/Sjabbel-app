ad0366deadcbddf5afbadade6f67797d36e210e5
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-05-27 16:01:42 +0200

    New stuff

---
+++ Sjabbel/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylehome.css">
+</head>
+<body>
+<div class="home flex-col-hstart-vstart clip-contents">
+      <div class="group-4 flex-col">
+        <p class="txt-957">Summerparty 2022</p>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-14%3A47?alt=media&token=c015c72b-d159-4e3d-b0d7-a2faf0e89b13"
+          alt="Not Found"
+          class="rectangle-3"
+        />
+        <p class="txt-673">
+          Hey partypeople! Na een succesvolle Summerbarty zijn we terug met onze
+          SUMMERPARTY! Om dit te vieren zijn er niet 1 maar wel 2 avonden waarop
+          jullie meer dan welkom zijn!....
+        </p>
+      </div>
+      <div class="group-601 flex-col-hcenter">
+        <div class="group-364 flex-row">
+          <div class="hamburger flex-col">
+            <div class="group-739 flex-row">
+              <div class="ellipse-3" />
+              <div class="ellipse-4" />
+            </div>
+            <div class="group-649 flex-row">
+              <div class="ellipse-3" />
+              <div class="ellipse-4" />
+            </div>
+          </div>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-372%3A82?alt=media&token=6e757a6e-f5dd-472c-8377-eab67ca9c6f3"
+            alt="Not Found"
+            class="user"
+          />
+        </div>
+        <div class="group-2 flex-col">
+          <p class="txt-957">Afterwork 50jaar SVR</p>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-14%3A24?alt=media&token=41a8b986-0db6-411e-b9a0-ce20eb8c99a4"
+            alt="Not Found"
+            class="rectangle-3"
+          />
+          <p class="txt-673">
+            De Scoete van Roi bestaat 50 jaar en dat moet gevierd worden! 🤩 Kom
+            genieten van een heerlijk hapje en drankje met zomerige deuntjes op
+            de achtergrond; sfeer gegarandeerd! 🕺🏼🍔🍹🍸🍺 Save the date!
+          </p>
+        </div>
+        <div class="group-3 flex-col">
+          <p class="txt-957">Stam Cafe</p>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-14%3A33?alt=media&token=4bef7355-5530-41ba-b553-3a96d81e9e71"
+            alt="Not Found"
+            class="rectangle-3"
+          />
+          <p class="txt-673">
+            Scoete van Roi wordt 50 jaar en dat betekend voor t'Zolderke
+            natuurlijk ook feest!! Wij nodigen alle leiding en oud-leiding uit
+            voor een gezellige avond outdoor-zolderke in de tuin van het
+            meisjeslokaal.
+          </p>
+        </div>
+        <div class="group-5 flex-col">
+          <p class="txt-957">Lorem Ipsum</p>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-372%3A72?alt=media&token=84f26de2-b8e6-4af2-a1be-a8a0df860485"
+            alt="Not Found"
+            class="rectangle-3"
+          />
+          <p class="txt-673">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. At tortor
+            facilisi hac tincidunt gravida sollicitudin nibh. Proin egestas
+            aliquet venenatis, pulvinar sed volutpat proin amet cras. Erat
+            vulputate nisi magna egestas scelerisque purus placerat morbi
+            faucibus. Elit dui ac vel sapien, sed in quis.
+          </p>
+        </div>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-16%3A53?alt=media&token=967278f2-7668-4dc3-8135-3c4059bc7f00"
+        alt="Not Found"
+        class="logo-wip-1"
+      />
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-13%3A23?alt=media&token=f788589e-efad-4228-b19f-77c7764dd355"
+        alt="Not Found"
+        class="group-1"
+      />
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-138%3A13?alt=media&token=5bc2e080-f9fd-447e-8c8a-ee9e271ed30a"
+        alt="Not Found"
+        class="bottom-bar"
+      />
+    </div>
+</body>
+</html>
