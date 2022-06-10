2e6061b9b20f4b50cee754df51027579a1b5c1e0
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-06-10 12:48:15 +0200

    added Past Events page of Profile (HTML/CSS)

---
+++ Sjabbel/profile2.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styleprofile2.css">
+</head>
+<body>
+<div class="home flex-col-hstart-vstart clip-contents">
+      <div class="group-601 flex-col-hcenter">
+        <div class="group-364">
+          <div class="hamburger flex-col">
+            <div class="circles flex-row">
+              <div class="ellipse-1" />
+              <div class="ellipse-2" />
+            </div>
+            <div class="group-649 flex-row">
+              <div class="ellipse-3" />
+              <div class="ellipse-4" />
+            </div>
+          </div>
+          <a href="profile.php">
+            <img
+                src="assets/Usersgr.png"
+                alt="Not Found"
+                class="user"
+            />
+          </a>
+        </div>
+
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/6o5xp34susd-16%3A53?alt=media&token=967278f2-7668-4dc3-8135-3c4059bc7f00"
+        alt="Not Found"
+        class="logo-wip-1"
+      />
+    
+      <div class="profile">
+            <div class="details">
+            <div class="pic"> </div>
+            <div class="more"></div>
+            <div class="name">TheDoctor14</div>
+            <div class="rname">Real Name: John Smith</div>
+            <div class="age">Age: 21</div>
+          </div>
+        <div class="neonbar">
+            <img class="nbarimg" src="assets/bar2.png" alt="bar">
+            <a class="current" href="profile.php">Current Events</a>
+            <a class="past" href="profile2.php">Past Events</a>
+        </div>
+        <div class="events">
+            <div class="event_new">
+                <p class="Cafe">t' Zolderke Stamcafé</p>
+                <p class="adress">Hoefkantweg 5 1860 Meise</p>
+                <p class="time">21h to 00h</p>
+            </div>
+            <div class="event_old">
+                <p class="Cafe">ODT</p>
+                <p class="adress">Meisestraat 16 1890 Londerzeel</p>
+                <p class="time">21h to 00h</p>
+            </div>
+        </div>
+      </div>
+
+      <div class="bar">
+        <div class="rect"> </div>
+        <a href="index.php">
+            <div class="home_button"></div>
+            <img src="assets/home.png" alt="home" class="homeimg">
+        </a>
+        <a href="games.php">
+          <img src="assets/Crosshair.png" alt="games" class="gamesimg">
+        </a>
+        <a href="map.php">
+            <img src="assets/Map.png" alt="map" class="mapimg">
+        </a>
+      </div>
+    </div>
+</body>
+</html> 
